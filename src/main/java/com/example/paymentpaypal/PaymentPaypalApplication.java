529ed827f37e6fa7e367d2bf624b33ba4898320b
AyushVerma2 <ayushverma2@gmail.com>
2020-02-15 17:32:39 +0800

    Initial commit

---
+++ src/main/java/com/example/paymentpaypal/PaymentPaypalApplication.java
@@ -0,0 +1,13 @@
+package com.example.paymentpaypal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentPaypalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentPaypalApplication.class, args);
+	}
+
+}
